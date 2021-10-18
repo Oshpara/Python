dc9ecd82d0193489afe1a43299439dedf77bddb6
Dmitry.Riabchevskikh <Oshpara5@gmail.com>
2021-10-18 13:43:35 +0500

    1 коммит

---
+++ Python/Python1.py
@@ -0,0 +1,2 @@
+for X in 'spam':
+    print (X)
---
+++ Python/Script1.py
@@ -0,0 +1,8 @@
+# Первый сценарий Python
+
+import sys # Загрузить библиотечный модуль
+print (sys.platform)
+print (2 ** 8) # Возвести в степень
+x = 'spam!'
+print (x * 8) # Повторить строку
+print ("Hello World!")
