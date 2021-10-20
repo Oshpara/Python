9d8af8752f281a285927c17ce4bd9b58d73524f8
Dmitry.Riabchevskikh <Oshpara5@gmail.com>
2021-10-20 21:02:43 +0500

    3 коммит

---
+++ Python/MyServer/Main.py
@@ -0,0 +1,33 @@
+import socket
+
+def start_my_server():
+    try:    
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(('127.0.0.1',2000))
+        server.listen(4)
+        while True:
+            print('Working!')
+            client_socket, address = server.accept()
+            data = client_socket.recv(1024).decode('utf-8')
+            #print(data)
+            content = load_page_from_get_request(data)
+            client_socket.send(content)
+            client_socket.shutdown(socket.SHUT_WR)
+    except KeyboardInterrupt:
+        server.close()
+        print('Helloy!')
+
+def load_page_from_get_request(request_data):
+    HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
+    HDRS_404 = 'HTTP/1.1 404 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
+    path = request_data.split(' ')[1]
+    response = ''
+    try:
+        with open('views'+path, 'rb') as file:
+            response = file.read()
+        return HDRS.encode('utf-8') + response
+    except FileNotFoundError:
+        return(HDRS_404 + 'Sorry').encode('utf-8')
+
+if __name__ == '__main__':
+    start_my_server()
